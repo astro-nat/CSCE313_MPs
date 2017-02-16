8492d1fc83a8427ce62bcb55f6c791d21667e1ab
Natalie Cluck <ncluck@Natalies-MacBook-Pro.local>
2017-02-16 10:57:53 -0600

    Finished Part 5

--- MP2/mp2_part5.cpp
+++ MP2/mp2_part5.cpp
@@ -16,8 +16,18 @@
 int main()
 {
     
+    pid_t childProcess;
+    pid_t parentProcess;
+    int success;
     
+    pid_t success_value = waitpid(-1, &success, 0);
     
+    if(fork() == 0) {
+        write(1,"World!\n",8);
+    }
+    else if (success_value != 0) {
+        write(1, "Hello ",7);
+    }
     
     exit(0);
 }
