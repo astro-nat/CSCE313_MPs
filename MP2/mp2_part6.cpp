644ec6a9880f1b363a7eb66ad0fff14ac4fcf66c
Megan Fischer <meganfischer1@att.net>
2017-02-18 00:49:33 -0600

    Part 6 is working

--- MP2/mp2_part6.cpp
+++ MP2/mp2_part6.cpp
@@ -17,41 +17,36 @@
 /* -------------------------------------------------------------------------- */
 /* Standard Library Includes                                                  */
 /* -------------------------------------------------------------------------- */
-/*
+
  #include <stdlib.h>
  #include <unistd.h>
  #include <sys/types.h>
- 
+ #include <cstring>
  #include <iostream>
  
- char *cmd1[] = { "/bin/ls -la", 0 };
+ char *cmd1[] = { "ls", "-la", NULL };
+ //for exec each parameter should be a separate string
  
  int main()
  {
  
  int fd[2], nbytes;
- //pid_t childpid;
- //pid_t parentpid;
  
- char string[] = "ls -la";
- char readbuffer[80];
+ //char string[] = "ls -la"; //unused variable
+ char readbuffer[2048]; // is a buffer size of 80 enough?
  
  pipe(fd);
  
- if(fork() == 0) { // child writes to pipe
+ pid_t pid = fork();
+ 
+ if(pid == 0) { // child writes to pipe
  
  // open the pipe, call exec here, write output from exec into pipe
  
+ dup2(fd[1],1); // stdout goes to fd[1]
  close(fd[0]); // read not needed
  
- dup(fd[1]);
- close(fd[1]);
- 
- //execl(cmd1[0],(char*)NULL);
- 
- write(fd[1], cmd1[0], strlen(cmd1[0])+1);
- //write(fd[1], cmd2[0], strlen(cmd2[0])+1);
- 
+ execvp(cmd1[0],cmd1); //execute command in child
  
  exit(0);
  }
@@ -61,95 +56,14 @@
  
  close(fd[1]); // write not needed
  
+ //read the output of the child to readbuffer using fd[0]
  nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
  
- std::cout << readbuffer << std::endl;
- execl(readbuffer, (char*)NULL);
- 
- close(fd[0]);
+ //std::cout << readbuffer << std::endl;
+ //write also outputs readbuffer
+ //output readbuffer to STDOUT
  write(1, readbuffer, nbytes);
- 
- //execvp(cmd1[0],cmd1);
- 
  }
  
  exit(0);
  }
- */
-
-
-
-
-
-
-/* -------------------------------------------------------------------------- */
-/* Developers: Natalie Cluck, Megan Fischer                                   */
-/* Project: CSCE-313 Machine Problem #2                                       */
-/*                                                                            */
-/* A program named mp2_part6.cpp which starts the command ”ls -la” using
- a fork() followed by an exec() (any of the exec functions will work).
- Use a UNIX pipe system call to send the output of ls -la back to the parent,
- read it using the read() function, and then write it to the console using the
- write() function. Note: the pipe will not work if you do not close and/or
- redirect the correct file descriptors. It’s up to you to figure out which
- ones those need to be.                                                       */
-
-/*                                                                            */
-/* File: ./mp2_part6.cpp                                                      */
-/* -------------------------------------------------------------------------- */
-
-/* -------------------------------------------------------------------------- */
-/* Standard Library Includes                                                  */
-/* -------------------------------------------------------------------------- */
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <iostream>
-#include <sys/types.h>
-
-
-//char *cmd1[] = { "/bin/ls -la", 0 };
-
-int main()
-{
-    
-    std::cout << "0" << std::endl;
-    
-    int fd[2], nbytes;
-    
-    char readbuffer[80];
-    
-    pipe(fd);
-    
-    pid_t pid = fork();
-    
-    if(pid == 0) {
-        
-        std::cout << "2" << std::endl;
-        
-        close(fd[1]);
-        dup2(fd[0], 0);
-        execl("/bin/ls","ls","-la",(char*)NULL);
-        
-        
-    }
-    else {
-        
-        std::cout << "1" << std::endl;
-        
-        close(fd[0]);
-        
-        while ((nbytes = read(fd[1], readbuffer, sizeof(readbuffer))) > 0){
-            //std::cout << "nybytes: " << nbytes << std::endl;
-            write(fd[1], readbuffer, nbytes);
-            //std::cout << readbuffer << std::endl;
-        }
-        
-        //std::cout << readbuffer << std::endl;
-        close(fd[1]);
-        
-        
-    }
-    
-    exit(0);
-}
