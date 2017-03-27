8997608eb60385db581831644a034b9b7fd0fd57
Natalie Cluck <nano724@tamu.edu>
2017-03-27 16:44:57 -0500

    FCFS almost working

--- MP5/FCFS.cpp
+++ MP5/FCFS.cpp
@@ -5,7 +5,12 @@
 //Make sure you did the ArrivalEarlierThan Comparator
 FCFS::FCFS(string file)
 {
-
+    extractProcessInfo(file);
+    
+    for (int i = 0; i < process_info.size(); i++) {
+        fcfs_queue.push(Process(get<0>(process_info[i]), get<1>(process_info[i]), get<2>(process_info[i])));
+    }
+    
 }
 
 void FCFS::print(){
@@ -18,6 +23,30 @@ void FCFS::print(){
 //Assuming the time the CPU starts working with process is system time 0
 void FCFS::schedule_tasks(){
 	
+    int system_time = 0;
+    bool all_complete = 0;
+    
+    while(all_complete == false) {
+        
+        cout << "System Time[" << system_time << "].........Process[PID=" << fcfs_queue.top().getPid() << "] is Running" << endl;
+        
+        Process p = fcfs_queue.top();
+        fcfs_queue.pop();
+        p.Run(1);
+        fcfs_queue.push(move(p));
+        
+        if(fcfs_queue.top().is_Completed()) {
+            cout << "System Time[" << system_time << "].........Process[PID=" << fcfs_queue.top().getPid() << "] finished its job!" << endl;
+            fcfs_queue.pop();
+        }
+        
+        system_time++;
+        
+        if(fcfs_queue.empty()) {
+            all_complete = true;
+        }
+    }
+    
 }
 
 FCFS::FCFS(){}
