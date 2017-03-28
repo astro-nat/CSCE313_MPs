8332f9d8716bdde277e1ce850ff848c4e9389ea2
Natalie Cluck <nano724@tamu.edu>
2017-03-28 18:16:22 -0500

    Round robin + RR times done

--- MP5/RoundRobin.cpp
+++ MP5/RoundRobin.cpp
@@ -42,33 +42,48 @@ void RoundRobin::schedule_tasks() {
 	//print();
     
     int system_time = 0;
+    int time_count = 0;
     int complete_count = 0;
     int quantum_count = 0;
     bool all_complete = false;
     
+    int wait_times[processVec.size()];
+    int response_times[processVec.size()];
+    
     while(all_complete == false) {
+        
+        system_time++;
+        
+        //cout << "// SYSTEM TIME // = " << system_time << endl;
+        
         for(int i = 0; i < processVec.size(); i++) {
             
+            //cout << "Arrival time of " << processVec[i]->getPid() << ": " << processVec[i]->get_arrival_time() << endl;
+            
+            //cout << "Burst time of " << processVec[i]->getPid() << ": " << processVec[i]->get_cpu_burst_time() << endl;
+            
             if(processVec[i]->get_arrival_time() <= system_time) {
+                
                 quantum_count = 1;
                 while(quantum_count <= time_quantum && !processVec[i]->is_Completed()) {
                     
-                    //cout << "Arrival time of " << processVec[i]->getPid() << ": " << processVec[i]->get_arrival_time() << endl;
-                    //cout << "System time: " << system_time << endl;
-                    
-                    cout << "System Time[" << system_time << "].........Process[PID=" << processVec[i]->getPid() << "] is Running" << endl;
+                    if(processVec[i]->get_cpu_burst_time() == processVec[i]->get_remaining_time()) {
+                        wait_times[i] = system_time;
+                    }
+                    cout << "System Time[" << time_count << "].........Process[PID=" << processVec[i]->getPid() << "] is Running" << endl;
                     processVec[i]->Run(1);
-                        
+                    time_count++;
+                    system_time = time_count;
+                    
                     if(processVec[i]->is_Completed()) {
-                        cout << "System Time[" << system_time << "].........Process[PID=" << processVec[i]->getPid() << "] finished its job!" << endl;
+                        cout << "System Time[" << time_count << "].........Process[PID=" << processVec[i]->getPid() << "] finished its job!" << endl;
                         complete_count++;
+                        response_times[i] = system_time;
                     }
                     
                     quantum_count++;
                 }
             }
-            
-            system_time++;
             
         }
     
@@ -76,6 +91,17 @@ void RoundRobin::schedule_tasks() {
             all_complete = true;
         }
     }
+    
+    int wait_time_sum = 0;
+    int response_time_sum = 0;
+    for(int i = 0; i < processVec.size(); i++) {
+        wait_time_sum += wait_times[i];
+        response_time_sum += response_times[i];
+    }
+    
+    cout << "Average wait time: " << wait_time_sum/processVec.size() << endl;
+    cout << "Average response time: " << response_time_sum/processVec.size() << endl;
+
 }
 
 //Fill in the procesVec with shared_ptr
