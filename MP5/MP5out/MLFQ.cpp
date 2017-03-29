e4dd9961f201d2244b6ee25cb9c2bfa3fcdc6d93
Natalie Cluck <nano724@linux2.cs.tamu.edu>
2017-03-29 17:35:55 -0500

    MLFQ changes but seg fault

--- MP5/MP5out/MLFQ.cpp
+++ MP5/MP5out/MLFQ.cpp
@@ -13,58 +13,72 @@ MLFQ::MLFQ(string file)
         return get<1>(t1) < get<1>(t2);
     });
 
+    /* NOTES FROM NAT
+    * with new process, add to Level 1 queue. If full, add to Level 2 queue. If full, add to Level 3 queue. If full, add to lowest level (infinite size).
+    
+    * when time quantum used up on given level, drop a level until it finds a spot
+     */
+
+    // initialize to 3 levels
+    upperLevels.resize(3);
+    upperLevels[0].resize(LEVEL1_CAPACITY);
+    upperLevels[1].resize(LEVEL2_CAPACITY);
+    upperLevels[2].resize(LEVEL3_CAPACITY);
+    
     // load upper levels
-    int system_time = 0;
+    int position = 0;
     for(int i = 0; i < process_info.size(); i++) {
         
         shared_ptr<Process> p = shared_ptr<Process>(new Process(get<0>(process_info[i]), get<1>(process_info[i]), get<2>(process_info[i])));
         
-        if(upperLevels[0].size() != LEVEL1_CAPACITY) {
+        // start at level 1
+        int quantum = 0;
+        while(position != LEVEL1_CAPACITY && !p->is_Completed() && quantum < LEVEL1_QUANTUM) {
+            
+            upperLevels[0][position] = p;
+            p->update_remaining_time(p->get_remaining_time() - 1);
+            position++;
+            
+            p->change_arrival_time(position);
+            degrade_process(p,0,position);
+            
+            quantum++;
+        }
+        
+        // if level 1 capacity or quantum reached, drop to level 2
+        while(upperLevels[1].size() != LEVEL1_CAPACITY && !p->is_Completed()) {
+            
             int quantum = 0;
-            while(quantum < LEVEL1_QUANTUM && p->get_remaining_time() != 0) {
-                upperLevels[0].push_back(p);
+            while(quantum < LEVEL2_QUANTUM && p->get_remaining_time() != 0) {
+                upperLevels[1].push_back(p);
                 p->update_remaining_time(p->get_remaining_time() - 1);
             }
-            degrade_process(p,2,upperLevels[1].size());
             
+            p->change_arrival_time(p->get_arrival_time() - LEVEL2_QUANTUM);
+            degrade_process(p,0,p->get_arrival_time());
         }
-        else {
-            // drop to level 2
-            if(upperLevels[1].size() != LEVEL2_CAPACITY) {
-                int quantum = 0;
+        
+        // if level 2 capacity or quantum reached, drop to level 3
+        while(upperLevels[2].size() != LEVEL1_CAPACITY && !p->is_Completed()) {
+            int quantum = 0;
                 while(quantum < LEVEL2_QUANTUM && p->get_remaining_time() != 0) {
-                    upperLevels[1].push_back(p);
+                    upperLevels[2].push_back(p);
                     p->update_remaining_time(p->get_remaining_time() - 1);
                 }
-                degrade_process(p,3,upperLevels[2].size());
+                p->change_arrival_time(p->get_arrival_time() - LEVEL3_QUANTUM);
+                degrade_process(p,0,p->get_arrival_time());
             }
-            else {
-                // drop to level 3
-                if (upperLevels[2].size() != LEVEL3_CAPACITY) {
-                    int quantum = 0;
-                    while(quantum < LEVEL3_QUANTUM) {
-                        upperLevels[2].push_back(p);
-                    }
-                }
-                else {
-                    lowestLevel.push(*p);
-                    system_time++;
-                }
-            }
+            // if level 3 capacity or quantum eached, drop to lowest level
+        
+        if(!p->is_Completed()) {
+            lowestLevel.push(*p);
+            update_last_process_fcfs(*p);
+            position++;
         }
     }
-    
-    /*
-    for(int i = 0; i < LEVEL1_CAPACITY; i++) {
-        upperLevels[i].push_back(shared_ptr<Process>(new Process(get<0>(process_info[i]), get<1>(process_info[i]), get<2>(process_info[i]))));
-    }
-    
-    // load lowest level
-    for (int i = 0; i < process_info.size(); i++) {
-        lowestLevel.push(Process(get<0>(process_info[i]), get<1>(process_info[i]), get<2>(process_info[i])));
-    }
-     */
 }
+
+
 //This function is used to keep track of the process who entered the fcfs queue most recently
 //The purpose is to properly adjust the upcoming process's arrival time
 //If the arrival time is changed, you can still check its original arrival time by accessing the 
@@ -89,7 +103,12 @@ void MLFQ::update_last_process_fcfs(Process process_to_fcfs) {
 int MLFQ::level_jump(shared_ptr<Process> p, unsigned int start_level, unsigned int pos, unsigned int goal_level) {
 	
     if(start_level != goal_level && goal_level > start_level) {
-        upperLevels[goal_level][pos] = p;
+        if(start_level >= 0 && start_level < 2) {
+            upperLevels[goal_level][pos] = p;
+        }
+        else if (start_level == 2) {
+            lowestLevel.push(*p);
+        }
     }
     
 	return 0;
@@ -113,11 +132,8 @@ int MLFQ::level_jump(shared_ptr<Process> p, unsigned int start_level, unsigned i
 //You can use level_jump() function here based on which level the process is going to jump
 void MLFQ::degrade_process(shared_ptr<Process> p, unsigned int level, unsigned int pos) {
 	
-    if(level == 2 || level == 3) {
-        upperLevels[level-1][pos] = p;
-    }
-    else if (level == 1) {
-        lowestLevel.push(*p);
+    if(pos < upperLevels[level+1].size()) {
+        level_jump(p,level,pos,level+1);
     }
 }
 
@@ -127,16 +143,27 @@ Make sure print out the timing information correctly
 */
 void MLFQ::schedule_tasks(){
 	
-    // with new process, add to Level 1 queue. If full, add to Level 2 queue. If full, add to Level 3 queue. If full, add to lowest level (infinite size).
+    int system_time = 0;
     
-    // when time quantum used up on given level, drop a level until it finds a spot
-    
-    for(int i = 0; i < process_info.size(); i++) {
-        //upperLevels[0].push_back(process_info[i]);
-        for(int i = 0; i < LEVEL1_QUANTUM; i++) {
-            
-            cout << "Runnin runnin runnin" << endl;
-        }
+    cout << "System Time[" << system_time << "].........First Level Queue Starts Work\n";
+    for(int i = 0; i < upperLevels[0].size(); i++) {
+        cout << "System Time[" << system_time << "].........Process[PID=" << upperLevels[0][i]->getPid() << "] is Running" << endl;
+        system_time++;
+    }
+    cout << "System Time[" << system_time << "].........Second Level Queue Starts Work\n";
+    for(int i = 0; i < upperLevels[1].size(); i++) {
+        cout << "System Time[" << system_time << "].........Process[PID=" << upperLevels[1][i]->getPid() << "] is Running" << endl;
+        system_time++;
+    }
+    cout << "System Time[" << system_time << "].........Third Level Queue Starts Work\n";
+    for(int i = 0; i < upperLevels[2].size(); i++) {
+        cout << "System Time[" << system_time << "].........Process[PID=" << upperLevels[2][i]->getPid() << "] is Running" << endl;
+        system_time++;
+    }
+    cout << "System Time[" << system_time << "].........FCFS Level Queue Starts Work";
+    for(int i = 0; i < lowestLevel.size(); i++) {
+        cout << "System Time[" << system_time << "].........Process[PID=" << lowestLevel.top().getPid() << "] is Running" << endl;
+        system_time++;
     }
     
 }
